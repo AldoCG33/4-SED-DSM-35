8d29968da5875bb0918850a7381f6611155ded8a
AldoCG33 <al222210655@gmail.com>
2023-07-20 23:13:04 -0600

    practica 2 LAYOUT CON CONTAINER DINAMICOS Y  NAVEGACION

--- Proyecto/layout/menu.php
+++ Proyecto/layout/menu.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template-->
     <link href="resource/css/sb-admin-2.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/07bf2ec53c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
@@ -49,7 +50,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="">
+                <a class="nav-link" href="indexusuarios.php">
                 <i class="fa-sharp fa-solid fa-house"></i>
                     <span>Inicio</span></a>
             </li>
@@ -63,13 +64,13 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-fw fa-table"></i>
+                <a class="nav-link" href="Estados.php">
+                <i class="bi bi-globe-americas"></i>
                     <span>Estados</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-fw fa-table"></i>
+                <a class="nav-link" href="Municipios.php">
+                <i class="bi bi-geo-fill"></i>
                     <span>Municipios</span></a>
             </li>
 
@@ -80,14 +81,14 @@
 
             
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-fw fa-table"></i>
+                <a class="nav-link" href="comentarios.php">
+                <i class="bi bi-journal-text"></i>
                     <span>Comentarios</span></a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-fw fa-table"></i>
+                <a class="nav-link" href="preguntas.php">
+                <i class="bi bi-question-lg"></i>
                     <span>Preguntas</span></a>
             </li>
 
